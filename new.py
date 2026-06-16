4e6c31d93c42651f72d1ee39be3b39c05fdc11c4
Aryan Vishwakarma <aryanvishwakarma0291@gmail.com>
2026-06-16 18:03:58 +0530

    Matplotlib , linechart , barchart , scatterplot , seaborn , histogram

--- new.py
+++ new.py
@@ -1043,15 +1043,84 @@ df = pd.DataFrame(data)
 # print(city_avg)
 
 # create new CSV file 
-df2 = pd.read_csv('students.csv')
-print(df2)
-print("-----------------")
-df2['Name'] , df2['City'], df2['Grade'] = df2['Name'].str.strip()  ,  df2['City'].str.replace('#', ''), df2['Grade'].str.replace('#','')
-df2['Marks'] = df2['Marks'].replace('$','')
-print(df2)
+# df2 = pd.read_csv('students.csv')
+# print(df2)
+# print("-----------------")
+# df2['Name'] , df2['City'], df2['Grade'] = df2['Name'].str.strip()  ,  df2['City'].str.replace('#', ''), df2['Grade'].str.replace('#','')
+# df2['Marks'] = df2['Marks'].replace('$','')
+# print(df2)
 
 
 # data cleaning 
 # df2.to_csv('clean_output.csv' , index=False) 
 
+
+#matplotlib
+
+import matplotlib.pyplot as plt
+
+# months = ['Jan' , 'Feb' , 'Mar' , 'Apr' , 'May' , 'June' , 'July' , 'Aug' , 'Sep' , 'Oct' , 'Nov' , 'Dec']
+# sales = [45,52,48,61,58,72,69,75,68,82,90,95]
+
+#line chart
+
+# plt.figure(figsize=(12,5))
+# plt.plot(months,sales,marker = 'o' , color= 'steelblue' , linewidth = 2 , markersize=8)
+# plt.fill_between(months,sales,alpha=0.15,color='steelblue')
+# plt.title('Monthly Sales 2026 (Rs. Thousands)', fontsize=14 , fontweight= 'bold')
+# plt.xlabel('Month')
+# plt.ylabel('Sales (Rs, K)')
+# plt.grid(True, alpha = 0.3)
+# plt.tight_layout()
+# plt.show()
+
+
+#Bar chart
+# cities = ['Bhopal' , 'Indore' , 'Jabalpur' , 'Gwalior' , 'Ujjain']
+# Students = [1200 , 2000 , 900 , 850 , 600]
+# colors = ['#2196F3' , '#4CAF50' , '#FF9800' , '#9C27B0' , '#F44336']
+
+# #BAr chart
+# plt.figure(figsize=(9,5))
+# bars = plt.bar(cities,Students, color=colors, edgecolor='white',linewidth=1.5)
+# plt.title('Students enrolled per city')
+# plt.ylabel('Number of Students')
+# for bar,val in zip(bars,Students):
+#       plt.text(bar.get_x() + bar.get_width()/2, val+30 , str(val) , ha='center',fontweight='bold')
+# plt.tight_layout()
+# plt.show()
+
+#scatter plot
+# import numpy as np
+
+# study_hrs = np.random.uniform(2,10,50)
+# marks = study_hrs*7+np.random.normal(0,8,50)
+# marks = np.clip(marks,30,100)
+
+# plt.figure(figsize=(8,5))
+# plt.scatter(study_hrs,marks,c=marks,cmap='RdYlGn', s=100, alpha=0.8)
+# plt.colorbar(label='Marks')
+# plt.title('Study Hours vs Exam Marks')
+# plt.xlabel('Study Hours/Day')
+# plt.ylabel('Exam Marks')
+# plt.show()
+
+#seaborn library
+# import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+
+# df = pd.DataFrame({
+#     'marks': np.random.randint(40, 100, 100),
+#     'study_hours': np.random.uniform(2, 10, 100),
+#     'city': np.random.choice(['Bhopal', 'Indore', 'Jabalpur'], 100),
+#     'gender': np.random.choice(['Male', 'Female'], 100)
+# })
+
+# # Histogram
+# plt.figure(figsize=(10, 4))
+# sns.histplot(df['marks'], bins=20, kde=True, color='steelblue')
+# plt.title('Distribution of Student Marks')
+# plt.show()
 
